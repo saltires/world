00212aa8f048a8302eb4fc4e40f4e05a43a2d71e
saltires <ye998102@163.com>
2020-12-11 00:09:36 -0800

    lll

--- cli/exercise-cli/index.js
+++ cli/exercise-cli/index.js
@@ -4,3 +4,6 @@ console.log('谢邀，人在美国，刚下飞机。');
 console.log('谢邀，人在美国，刚下飞机。');
 console.log('谢邀，人在美国，刚下飞机。');
 console.log('谢邀，人在美国，刚下飞机。');
+console.log('谢邀，人在美国，刚下飞机。');
+console.log('谢邀，人在美国，刚下飞机。');
+console.log('谢邀，人在美国，刚下飞机。');
