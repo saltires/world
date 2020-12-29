9b5114de201dd24e6bfbc9b50ceeea8cf895070b
yejianxin <yejianxin@rightknights.com>
2020-12-29 10:27:14 +0800

    feat: 非框架模式下，为单独的 js 文件增加 HMR

--- webpack/webpack-hmr/src/index.js
+++ webpack/webpack-hmr/src/index.js
@@ -8,6 +8,10 @@ const img = new Image()
 
 img.src = yby
 
+module.hot.accept('./render.js', () => {
+    console.log('render.js 内容变更了')
+})
+
 const heading = createHead()
 
 document.body.append(heading)
